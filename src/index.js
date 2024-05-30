4a31520523f7f0b0dc282728949f8ed130c31eb9
rossiemilmr.30@gmail.com <rossiemilmr.30@gmail.com>
2024-05-30 14:40:55 -0500

    inicio pagina index y menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 
 /* -------------------------- Import ------------------------- */
 import express from 'express';
+import morgan from 'morgan';
+import {join, dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {engine} from 'express-handlebars'
+
 /* ----------------------------- initialization ----------------------------- */
 const app = express();
+
+/* ----------------- Evitar colisiones de nombre de variable ---------------- */
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /* --------------------------------- setting -------------------------------- */
 app.set('port', process.env.PORT || 3000 );
+app.set('views', join (__dirname, 'views'))
+app.engine('.hbs', engine ({
+    defaultlayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
 /* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 /* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res) => {
+    res.render("index")
+});
 /* ------------------------------ Public files ------------------------------ */
+app.use(express.static(join(__dirname, 'public')))
 /* ------------------------------- Run server ------------------------------- */
 app.listen(app.get('port'), () => {
     console.log('server listening on port', app.get('port'));
