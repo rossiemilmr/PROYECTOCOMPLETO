9d016a43e5818953f1c47a802a85cb67d2ace1cb
rossiemilmr.30@gmail.com <rossiemilmr.30@gmail.com>
2024-05-30 19:43:31 -0500

    Se realizo configuracion de la base de datos

---
+++ src/database/query.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE prueba01;
+
+USE prueba01;
+
+CREATE TABLE personas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    age INT
+);
+
+SELECT * FROM personas;
+
+CREATE USER 'prueba01'@'localhost'IDENTIFIED BY 'prueba01';
+GRANT ALL PRIVILEGES ON prueba01.*to 'prueba01'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'prueba01'@'localhost';
