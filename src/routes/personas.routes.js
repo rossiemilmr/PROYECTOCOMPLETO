e40e75e639e11e67e55b84340a741262e284d6ae
rossiemilmr.30@gmail.com <rossiemilmr.30@gmail.com>
2024-05-31 21:14:49 -0500

    Se creo el boton eliminar

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -7,8 +7,18 @@ const router = Router();
 router.get('/list', async(req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM personas');
-        res.render('personas/list', {personas:result})
+        res.render('personas/list', {personas: result})
     }catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.get('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+        res.redirect('/list');
+    } catch (error) {
         res.status(500).json({message: error.message});
     }
 });
